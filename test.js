a2f1719023d948fe48e3413109d6a3a6b01b785e
Lapizzerina <lapizzerina@gmail.com>
2026-09-25 15:38:04 +0200

    Align Zadarma phone tests with Odoo partner schema

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ for (const phone of ["603509208", "+34603509208", "0034603509208", "+34 603 509 
 assert.equal(normalizePhoneE164("+33 6 12 34 56 78"), "+33612345678");
 assert.equal(normalizePhoneE164(""), null);
 assert.equal(partnerMatchesPhone({ phone: "+34 603 509 208", mobile: false }, "+34603509208"), true);
-assert.equal(partnerMatchesPhone({ phone: false, mobile: "0034603509208" }, "+34603509208"), true);
+assert.equal(partnerMatchesPhone({ phone: "0034603509208" }, "+34603509208"), true);
 assert.equal(partnerMatchesPhone({ phone: "+34600000000", mobile: false }, "+34603509208"), false);
 
 console.log("Phone normalization tests passed");
